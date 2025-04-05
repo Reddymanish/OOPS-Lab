24c6e0a86ccdbdb4dd2b32fa033d7ec3abe15906
Reddymanish <reddymanish9988@gmail.com>
2025-04-05 14:57:39 +0530

    Add files via upload

---
+++ Inheritence Programes/Multi-Level Inheritence/Multilevel1.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+class Vehicle {
+    String brand;
+    int wheels;
+
+    Vehicle(String brand, int wheels) {
+        this.brand = brand;
+        this.wheels = wheels;
+    }
+
+    void showVehicleDetails() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Number of Wheels: " + wheels);
+    }
+}
+
+class Car extends Vehicle {
+    String fuelType;
+
+    Car(String brand, int wheels, String fuelType) {
+        super(brand, wheels);
+        this.fuelType = fuelType;
+    }
+
+    void showCarDetails() {
+        showVehicleDetails();
+        System.out.println("Fuel Type: " + fuelType);
+    }
+}
+
+class SportsCar extends Car {
+    int maxSpeed;
+
+    SportsCar(String brand, int wheels, String fuelType, int maxSpeed) {
+        super(brand, wheels, fuelType);
+        this.maxSpeed = maxSpeed;
+    }
+
+    void showSportsCarDetails() {
+        showCarDetails();
+        System.out.println("Max Speed: " + maxSpeed + " km/h");
+    }
+}
+
+public class Multilevel1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Car Brand: ");
+        String brand = sc.nextLine();
+        System.out.print("Enter Number of Wheels: ");
+        int wheels = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter Fuel Type: ");
+        String fuelType = sc.nextLine();
+        System.out.print("Enter Max Speed: ");
+        int maxSpeed = sc.nextInt();
+
+        SportsCar sportsCar = new SportsCar(brand, wheels, fuelType, maxSpeed);
+
+        System.out.println("\nSports Car Details:");
+        sportsCar.showSportsCarDetails();
+
+        sc.close();
+    }
+}
